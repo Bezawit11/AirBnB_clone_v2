a9fdafe91a728a7f6c65fa6c2d75c73bbad5bf88
binael <99293451+binael@users.noreply.github.com>
2023-01-06 21:35:11 +0100

    Added comments

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
+""" Module for review test cases"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ Class for the test cases of base review"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialization of attributes"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ Method to test for place id"""
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertNotEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ Method to test for user id"""
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertNotEqual(type(new.user_id), str)
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertNotEqual(type(new.text), str)
