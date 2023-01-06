c44836410fef68ddc5c6ceabebf4b9fb0beea2d8
binael <99293451+binael@users.noreply.github.com>
2023-01-06 21:24:41 +0100

    Update test_state.py
    Update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" Module that implements the test cases for State"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ The class test for AirBnB states"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialize the attributes"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ Method that validates and tests for name"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertNotEqual(type(new.name), str)
