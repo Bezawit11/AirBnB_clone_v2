be8d2c5932d3265682c9fa8488ceb63e09c4341d
Bezawit11 <socbeza13@gmail.com>
2023-02-06 05:33:21 +0000

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hello():
 @app.route('/c/<text>', strict_slashes=False)
 def C_is_fun():
     """display the text variable"""
-    return 'C %s' % escape(text.replace("_", " "))
+    return 'C %s' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
