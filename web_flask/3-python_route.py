dd175f27add6aa946f918cdc393e6151f49884a7
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-07 06:47:59 +0000

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,15 +11,18 @@ def hello_hbnb():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello():
     """display hbnb"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def C_is_fun(text):
     """display the text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
