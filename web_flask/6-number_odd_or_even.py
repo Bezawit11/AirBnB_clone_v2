be4f8c20bc5b406f2d8be453f294aa7e8c6aae1e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-07 06:52:40 +0000

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -7,20 +7,24 @@ a script that starts a Flask web application
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
     """display hbnb"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def C_is_fun(text):
     """display the text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -30,15 +34,18 @@ def Python_is_fun(text=None):
         text = "is cool"
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_integer(n):
     """displays a number"""
     return str(n) + ' is a number'
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def is_int(n):
     """displays a number"""
     return render_template("5-number.html", n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
