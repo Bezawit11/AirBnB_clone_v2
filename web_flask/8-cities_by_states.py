76946961a6b21cdab8825d3c0287149415bcd6e2
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-07 07:41:48 +0000

    Update 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_cities():
+    """lists states from database"""
+    z_states = storage.all(State)
+    all_states = []
+    for key, value in z_states.items():
+        all_states.append(value)
+    return render_template('8-cities_by_states.html', all_states=all_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    
